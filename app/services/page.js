18b6bea592447b178d4b916fccf74f4b0c3c1589
Quinnfickle <quinne.walker@gmail.com>
2025-04-15 23:11:35 +1200

    prepare for redeployment

--- app/services/page.js
+++ app/services/page.js
@@ -6,7 +6,7 @@ export default function Services() {
         <ul>
           <li>House Wash: Professional exterior cleaning for your home.</li>
           <li>Driveway Wash: Get rid of stains and dirt with our high-pressure washing.</li>
-          <li>Window Cleaning: Crystal-clear windows that make your home shine.</li>
+          <li>Window Cleaning: Crystal clear windows that make your home shine.</li>
         </ul>
       </div>
     );
